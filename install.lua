5122eb0c08f651833b1016af1fac9bc255058feb
PlutoniumMuncher <teddybear112623@gmail.com>
2025-03-21 18:09:14 -0400

    Update install.lua
    Changed install link

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/coaber/DEReactor/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/coaber/DEReactor/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/PlutoniumMuncher/Better-DER/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/PlutoniumMuncher/Better-DER/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
